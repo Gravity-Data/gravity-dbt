5af97c2c437f0afbfe59c19d9b0bc6dadd9e197f
david.jayatillake@gravitydata.co <david.jayatillake@gravitydata.co>
2021-09-30 19:19:48 +0000

    sources model created

---
+++ models/sources/sources.sql
@@ -0,0 +1,9 @@
+{{ config(materialization = 'view') }}
+
+select distinct 
+    jobname, 
+    sourcename, 
+    organisationid,
+    value as release_status 
+from {{ source('raw', 'truedash_conf_datasources') }}  
+where organisationid>1000 and parameter='RELEASE_STATUS' 
