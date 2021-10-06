9d1caf50121a13e7ee37dd8eb0ebcb4660a3cbf5
Lauren Mulholland <lauren.mulholland@gravitydata.co>
2021-10-06 15:16:16 +0000

    release status columns in models

--- models/targets/targets.sql
+++ models/targets/targets.sql
@@ -1,7 +1,7 @@
 {{ config(materialized='view') }}
 
 select 
-distinct connectionname, targetname, organisationid,parameter,value
+distinct connectionname, targetname, organisationid, value as release_status
 from {{ source('raw', 'truedash_conf_datatargets') }}  
 where organisationid>1000 
 and parameter='RELEASE_STATUS'
