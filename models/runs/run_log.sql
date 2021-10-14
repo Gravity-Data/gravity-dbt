674d965bbabc4a59097ddb3e4b3ce89cba02bb05
david.jayatillake@gravitydata.co <david.jayatillake@gravitydata.co>
2021-10-14 09:27:55 +0000

    adding-duration-calc-field-in-run-log

--- models/runs/run_log.sql
+++ models/runs/run_log.sql
@@ -1,4 +1,17 @@
 {{ config(materialized='view') }}
 
-select *
+select
+      jobname,
+      sourcename,
+      resourceid,
+      run_id,
+      start_datetime,
+      end_datetime,
+      datetime_diff(end_datetime, start_datetime, second) duration_sec,
+      status,
+      error_message,
+      staged_records_count,
+      new_records_count,
+      updated_records_count,
+      organisationid
 from {{ source('raw', 'DE_Scheduler_Run_Log') }}
