67cef2b4a3e27374ca170c1e985ee50f232add5a
Sameer Keluskar <sameer.keluskar@gravitydata.co>
2022-04-28 13:26:23 +0000

    test

--- tests/demo_test.sql
+++
@@ -1,8 +0,0 @@
-select distinct
-    {{ dbt_utils.surrogate_key(['jobname', 'sourcename','organisationid']) }} as source_id,
-    jobname, 
-    sourcename, 
-    organisationid,
-    value as release_status
-from {{ source('raw', 'truedash_conf_datasources') }}  
-where organisationid=100005
